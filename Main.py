0003ee5c4d7636520ef288ef64b69c68f694629f
samvera-spec <samuel.vera@ciencias.unam.mx>
2025-02-15 02:09:21 -0600

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,11 @@
+#Fecha: 14 de febrero de 2025
+#@version: 1.1
+#@author: Samuel Vera
+#En esta clase es en donde se ejecuta el codigo, para que asi nos de los datos que nosostros requerimos.
+
+from Cuenta import *
+class Pruebas:
+  pass
+  print("imprimir atributos")
+  Cuenta 1= Cuenta(saldo, "tipo", "nombre")
+  Cuenta 1. imprimirDetalles()
