94bcd11bb48e39c463cee19ce9975ee5ff3c9f45
samvera-spec <samuel.vera@ciencias.unam.mx>
2025-02-18 17:32:06 -0600

    Create Main.py

---
+++ Teoria/Clases y Metodos/Main.py
@@ -0,0 +1,23 @@
+#Fecha: 18 de febrero de 2025
+#@version: 1.1
+#@author: Samuel Vera
+#En esta clase es en donde se ejecuta el codigo, para que asi nos de los datos que nosostros requerimos.
+
+
+from Cuenta import Cuenta
+class Main:
+    pass
+menu = Menu()
+menu.imprimirDetalles() 
+cliente1 = Cliente("nombre", "direccion", "edad")
+cliente1.imprimirDetalles() 
+cuenta1 = Cuenta(7000, "tipo", "nombre")
+cuenta1.imprimirDetalles()
+cuenta1. retirar(400)
+print("Retiro exitoso de:", 400)
+print(cuenta1.saldo)
+cuenta1.imprimirDetalles()
+cuenta1. depositar(1000)
+print("Depósito exitoso de:", 1000)
+print(cuenta1.saldo)
+cuenta1.imprimirDetalles()
