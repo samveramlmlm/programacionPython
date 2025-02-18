d3908fabe0ec2c56942fd0a79342ca6f73b1b752
samvera-spec <samuel.vera@ciencias.unam.mx>
2025-02-18 17:04:27 -0600

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,4 +1,4 @@
-#Fecha: 14 de febrero de 2025
+#Fecha: 18 de febrero de 2025
 #@version: 1.1
 #@author: Samuel Vera
 #En esta clase es en donde se da el mensaje de bienvendida, al propietario de una cuenta bancaria, es lo primero que ve el usuario.
