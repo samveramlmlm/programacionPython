5af6e60b6a08c1de89ed1749467495e795197166
samvera-spec <samuel.vera@ciencias.unam.mx>
2025-02-18 17:29:34 -0600

    Update Menu.py

--- Teoria/Clases y Metodos/Menu.py
+++ Teoria/Clases y Metodos/Menu.py
@@ -1,1 +1,11 @@
+#Fecha: 18 de febrero de 2025
+#@version: 1.1
+#@author: Samuel Vera
+#En esta clase es en donde se da el mensaje de bienvendida, al propietario de una cuenta bancaria, es lo primero que ve el usuario.
 
+class Menu:
+    def __init__(self):
+        pass
+
+    def imprimirDetalles(self):
+        print("Bienvenido a Santander")
