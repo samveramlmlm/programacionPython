d097b87f21c4343072d274ef70730bc0536932aa
samvera <samuel.vera@ciencias.unam.mx>
2025-02-19 19:08:04 -0600

    Update Main.py

--- Teoria/Clases y Metodos/Main.py
+++ Teoria/Clases y Metodos/Main.py
@@ -5,18 +5,23 @@
 
 
 from Cuenta import Cuenta
+
 class Main:
     pass
+
 menu = Menu()
 menu.imprimirDetalles() 
+
 cliente1 = Cliente("nombre", "direccion", "edad")
 cliente1.imprimirDetalles() 
+
 cuenta1 = Cuenta(7000, "tipo", "nombre")
 cuenta1.imprimirDetalles()
 cuenta1. retirar(400)
 print("Retiro exitoso de:", 400)
 print(cuenta1.saldo)
 cuenta1.imprimirDetalles()
+
 cuenta1. depositar(1000)
 print("Depósito exitoso de:", 1000)
 print(cuenta1.saldo)
