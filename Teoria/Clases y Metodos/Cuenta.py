916c517d02303670011c0c4735b64efc4e6cd3fa
samvera <samuel.vera@ciencias.unam.mx>
2025-02-19 19:06:18 -0600

    Update Cuenta.py

--- Teoria/Clases y Metodos/Cuenta.py
+++ Teoria/Clases y Metodos/Cuenta.py
@@ -4,16 +4,19 @@
 #En esta clase se almacenan los atributos y los procesos de la cuenta.
 
 class Cuenta:
+    
     def __init__(self, saldo, tipo, nombre):
         self.saldo=saldo
         self.tipo=tipo
         self.nombre=nombre
+    
     def imprimirDetalles(self):
-        print("Desde el metodo")
         print("saldo:", self.saldo)
         print("tipo:", self.tipo)
         print("nombre:", self.nombre)
+    
     def retirar(self, cantidad):
         self.saldo = self.saldo-cantidad
+    
     def depositar(self, cantidad):
         self.saldo = self.saldo+cantidad
