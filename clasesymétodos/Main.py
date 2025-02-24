439a99a9589cda562b0f60ba7ddd0dee687125e2
samvera <samuel.vera@ciencias.unam.mx>
2025-02-24 11:23:19 -0600

    Update Main.py

--- clasesymétodos/Main.py
+++ clasesymétodos/Main.py
@@ -3,3 +3,14 @@ from Cuenta import Cuenta
 class Main:
     pass
   
+cuenta1 = Cuenta(7000, "tipo", "nombre")
+cuenta1.imprimirDetalles()
+cuenta1. retirar(400)
+print("Retiro exitoso de:", 400)
+print(cuenta1.saldo)
+cuenta1.imprimirDetalles()
+
+cuenta1. depositar(1000)
+print("Depósito exitoso de:", 1000)
+print(cuenta1.saldo)
+cuenta1.imprimirDetalles()
