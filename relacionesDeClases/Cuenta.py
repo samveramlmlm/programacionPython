bbbdfdf46814bbf2d7275b31b132c9cf40c951bc
samvera <samuel.vera@ciencias.unam.mx>
2025-03-08 18:45:55 -0600

    Update and rename Cuenta,py to Cuenta.py

---
+++ relacionesDeClases/Cuenta.py
@@ -0,0 +1,22 @@
+class Cuenta:
+    
+    def __init__(self, saldo, tipo, nombre):
+        self.saldo = saldo
+        self.tipo = tipo
+        self.nombre = nombre
+    
+    def imprimirDetalles(self):
+        print("Saldo:", self.saldo)
+        print("Tipo de cuenta:", self.tipo)
+        print("Nombre del titular:", self.nombre)
+    
+    def retirar(self, cantidad):
+        if cantidad > self.saldo:
+            print("Fondos insuficientes para retirar.")
+        else:
+            self.saldo -= cantidad
+            print(f"Has retirado {cantidad}. Nuevo saldo: {self.saldo}")
+
+    def depositar(self, cantidad):
+        self.saldo += cantidad
+        print(f"Has depositado {cantidad}. Nuevo saldo: {self.saldo}")
