754bab22476370e55c7193b63f7a07bf581b15b9
samvera <samuel.vera@ciencias.unam.mx>
2025-03-12 17:27:35 -0600

    Update Main.py

--- relacionesDeClases/Main.py
+++ relacionesDeClases/Main.py
@@ -1,3 +1,9 @@
+#Fecha: 10 de Marzo de 2025
+#@version: 1.2
+#@author: Samuel Vera
+#En esta clase es en donde se ejecuta el codigo, para que asi nos de los datos que nosostros requerimos.
+
+
 from Menu import Menu
 from Cliente import Cliente
 from Cuenta import Cuenta
