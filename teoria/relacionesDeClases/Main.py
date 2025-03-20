b8a41a6f5797200788f637fcff7a1b42dd9af2fe
samvera <samuel.vera@ciencias.unam.mx>
2025-03-20 16:30:27 -0600

    Update Main.py

--- teoria/relacionesDeClases/Main.py
+++ teoria/relacionesDeClases/Main.py
@@ -1,4 +1,4 @@
-#Fecha: 10 de Marzo de 2025
+#Fecha: 19 de Marzo de 2025
 #@version: 1.2
 #@author: Samuel Vera
 #En esta clase es en donde se ejecuta el codigo, para que asi nos de los datos que nosostros requerimos.
@@ -8,25 +8,22 @@ from Menu import Menu
 from Cliente import Cliente
 from Cuenta import Cuenta
 
-class Main: 
-    
-        menu = Menubanco("Bienvenid@ a Menú Bancario")
-        menu.darBienvenida()
-        menu.desplegarOpciones()
+if __name__ == "__main__":
+    menu = Menu("Bienvenid@ a Menú Bancario")
+    menu.darBienvenida()
+    menu.desplegarOpciones()
 
-        cliente1 = Cliente("nombre", "direccion", "edad", "cuenta")
-        cliente1.imprimirDetalles()
+    # Crear cuenta y cliente
+    cuenta1 = Cuenta(1000, "Ahorros")
+    cliente1 = Cliente("Sam Vera", "Calle 160", 18, cuenta1)
 
-        cuenta1 = Cuenta(7000, "tipo")
-        cuenta1.imprimirDetalles()
+    if cuenta1.retirar(400): 
+        print("Retiro exitoso de: 400")
+    else:
+        print("Saldo insuficiente para el retiro.")
 
-    
-        if cuenta1.retirar(400): 
-            print("Retiro exitoso de: 400")
-        else:
-            print("Saldo insuficiente para el retiro.")
-        print(cuenta1.saldo)
-        cuenta1.imprimirDetalles()
+    cuenta1.depositar(1000)
+    print("Depósito exitoso de: 1000")
 
         
         cuenta1.depositar(1000)
