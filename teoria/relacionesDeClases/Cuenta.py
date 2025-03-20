f781427127bb8438467de50c3a50dd53909ed074
samvera <samuel.vera@ciencias.unam.mx>
2025-03-20 16:32:07 -0600

    Update Cuenta.py

--- teoria/relacionesDeClases/Cuenta.py
+++ teoria/relacionesDeClases/Cuenta.py
@@ -1,25 +1,28 @@
-#Fecha: 10 de Marzo de 2025
+#Fecha: 19 de Marzo de 2025
 #@version: 1.2
 #@author: Samuel Vera
 #En esta clase se almacenan los atributos y los procesos de la cuenta.
 class Cuenta:
     
-    def __init__(self, saldo, tipo, nombre):
-        self.saldo = saldo
-        self.tipo = tipo
-        self.nombre = nombre
-    
-    def imprimirDetalles(self):
-        print("Saldo:", self.saldo)
-        print("Tipo de cuenta:", self.tipo)
-    
+    def __init__(self, saldo, tipo):
+        self.__saldo = saldo
+        self.__tipo = tipo
+
+    def getSaldo(self):
+        return self.__saldo
+
+    def getTipo(self):
+        return self.__tipo
+
     def retirar(self, cantidad):
-        if cantidad > self.saldo:
+        if cantidad > self.__saldo:
             print("Fondos insuficientes para retirar.")
+            return False
         else:
-            self.saldo -= cantidad
-            print(f"Has retirado {cantidad}. Nuevo saldo: {self.saldo}")
+            self.__saldo -= cantidad
+            print(f"Has retirado ${cantidad:.2f}. Nuevo saldo: ${self.__saldo:.2f}")
+            return True
 
     def depositar(self, cantidad):
-        self.saldo += cantidad
-        print(f"Has depositado {cantidad}. Nuevo saldo: {self.saldo}")
+        self.__saldo += cantidad
+        print(f"Has depositado ${cantidad:.2f}. Nuevo saldo: ${self.__saldo:.2f}")
