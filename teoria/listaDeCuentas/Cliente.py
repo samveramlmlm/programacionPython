d27407a59af0697d6f5eb57f49cf2fe7b5953a65
samvera <samuel.vera@ciencias.unam.mx>
2025-04-06 17:39:38 -0600

    Update Cliente.py

--- teoria/listaDeCuentas/Cliente.py
+++ teoria/listaDeCuentas/Cliente.py
@@ -1,1 +1,15 @@
+#Fecha: 19 de Marzo de 2025
+#@version: 1.2
+#@author: Samuel Vera
+#En esta clase se almacenan algunas propiedades de los cliente (atributos) y tambien algunos procesos que los mismo llevan a cabo, como lo serian el retiro o el deposito.
+from Cuenta import Cuenta
 
+class Cliente:
+    
+    def __init__(self, nombre, direccion, edad, cuenta):
+        self.__nombre = nombre
+        self.__direccion = direccion
+        self.__edad = edad
+        self.cuenta = cuenta  
+
+     
