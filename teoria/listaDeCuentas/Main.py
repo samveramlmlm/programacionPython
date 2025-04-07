3eccf7c8ddda044f031da5ad4c8ce09447d547c2
samvera <samuel.vera@ciencias.unam.mx>
2025-04-06 21:38:28 -0600

    Update Main.py

--- teoria/listaDeCuentas/Main.py
+++ teoria/listaDeCuentas/Main.py
@@ -1,27 +1,23 @@
-# Fecha: 19 de Marzo de 2025
-# @version: 1.2
+# Fecha:2 de Abril de 2025
+# @version: 1.3
 # @autor: Samuel Vera
 # En esta clase es en donde se ejecuta el código, para que así nos dé los datos que nosotros requerimos.
 
-from Menu import Menu
 from Cliente import Cliente
 from Cuenta import Cuenta
+from Menu import Menu
 
 if __name__ == "__main__":
-    menu = Menu("Bienvenid@ a Menú Bancario")
+    # Crear un cliente y cuentas
+    cliente1 = Cliente("Sam Vera", "Calle 160", 18)
+    cuenta1 = Cuenta(1000, "Ahorros")
+    cuenta2 = Cuenta(2000, "Corriente")
+    
+    # Agregar cuentas al cliente
+    cliente1.agregarCuenta(cuenta1)
+    cliente1.agregarCuenta(cuenta2)
+
+    # Crear y mostrar el menú
+    menu = Menu("Bienvenid@ a Menú Bancario", cliente1)
     menu.darBienvenida()
     menu.desplegarOpciones()
-
-    cuenta1 = Cuenta(1000, "Ahorros")
-    cliente1 = Cliente("Sam Vera", "Calle 160", 18, cuenta1)
-
-    if cuenta1.retirar(400): 
-        print("Retiro exitoso de: 400")
-    else:
-        print("Saldo insuficiente para el retiro.")
-
-    cuenta1.depositar(1000)
-    print("Depósito exitoso de: 1000")
-    cuenta1.mostrarSaldo()
-    cuenta1.imprimirDetalles()
-
