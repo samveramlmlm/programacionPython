5468f650b15a321379d74f24c1b5e567cfe647b2
samvera <samuel.vera@ciencias.unam.mx>
2025-04-06 21:35:44 -0600

    Update Cuenta.py

--- teoria/listaDeCuentas/Cuenta.py
+++ teoria/listaDeCuentas/Cuenta.py
@@ -1,5 +1,5 @@
-#Fecha: 19 de Marzo de 2025
-#@version: 1.2
+#Fecha: 2 de Abril de 2025
+#@version: 1.3
 #@author: Samuel Vera
 #En esta clase se almacenan los atributos y los procesos de la cuenta.
 class Cuenta:
@@ -26,3 +26,13 @@ class Cuenta:
     def depositar(self, cantidad):
         self.__saldo += cantidad
         print(f"Has depositado ${cantidad:.2f}. Nuevo saldo: ${self.__saldo:.2f}")
+
+    def imprimirDetalles(self):
+        print(f"Tipo de cuenta: {self.__tipo}")
+        print(f"Saldo actual: ${self.__saldo:.2f}")
+
+    def mostrarSaldo(self):
+        print(f"Saldo actual: ${self.__saldo:.2f}")
+
+    def __str__(self):
+        return f"Cuenta tipo: {self.__tipo}, Saldo: ${self.__saldo:.2f}"
